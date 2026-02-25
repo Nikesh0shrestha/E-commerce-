474570f43bf60bd250319a11cfb451547f43825f
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-02-25 09:41:30 +0545

    Change in Database

--- shopsphere/settings.py
+++ shopsphere/settings.py
@@ -89,10 +89,22 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',
+        'USER': 'postgres',
+        'PASSWORD': 'nikesh@26',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
